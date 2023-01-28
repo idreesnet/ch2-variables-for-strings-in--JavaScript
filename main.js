cdd2992fd8be84a680931155078347de029a25d1
idreesnet <54152894+idreesnet@users.noreply.github.com>
2023-01-28 20:31:41 +0500

    Add files via upload
    Chapter 2 assignment's files added.

---
+++ main.js
@@ -0,0 +1,62 @@
+/* Question 1
+ ----------------*/
+
+// var username;
+// username = "idrees";
+// alert(username);
+
+
+/* Question 2
+----------------*/
+// var myName = "Muhammad Idrees Ishaq bin Manzoor Ahmed bin Muhammad Bota bin Deen Muhammad bin Nabi Bakhsh bin Ahmed";
+// alert(myName);
+
+
+
+/* Question 3
+-----------------*/
+// var message;
+// message = "Hello World";
+// alert(message);
+
+
+/* Question 4
+-----------------*/
+// var studentName = "Jhone Doe";
+// var studentAge = "15 years old";
+// var studentCertification = "Certified Mobile Application Development";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCertification);
+
+
+
+/* Question 5
+---------------*/
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(pizza);
+
+
+/* Question 6
+-----------------*/
+// var email;
+// email = "codebot.dsu@gmail.com";
+// alert("My email address is " + email);
+
+
+/* Question 7
+----------------*/
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+
+/* Question 8
+---------------*/
+// document.write("<p>Yah! I can write HTML content through JavaScript</p>");
+
+
+/* Question 9
+-----------------*/
+var endPrint =" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ";
+window.alert(endPrint);
+
